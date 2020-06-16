4541f098453bd948e00db78b1d1bed37ac4d45f7
Charles Coombs-Esmail <coombs.charles@gmail.com>
2020-06-15 22:39:13 -0700

    Add webRTC support for multiple browsers

--- frontend/components/Stream/Stream.jsx
+++ frontend/components/Stream/Stream.jsx
@@ -21,15 +21,57 @@ class Stream extends React.Component {
 
 
     componentDidMount() {
+      if (navigator.mediaDevices === undefined) {
+        navigator.mediaDevices = {};
+      }
+
+      if (navigator.mediaDevices.getUserMedia === undefined) {
+        navigator.mediaDevices.getUserMedia = function (constraints) {
+
+          // First get ahold of the legacy getUserMedia, if present
+          var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+          if (!getUserMedia) {
+            return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+          }
+
+          // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
+          return new Promise(function (resolve, reject) {
+            getUserMedia.call(navigator, constraints, resolve, reject);
+          });
+        }
+      }
+
+      
+
         this.video = document.getElementById("local-video")
         this.props.requestChannel(this.props.match.params.channelId).then(() => {
-            navigator.mediaDevices.getUserMedia({ audio: true, video: true })
-                .then(stream => {
-                    this.localStream = stream;
-                    this.video.srcObject = stream;
-                }).catch(error => { console.log(error) })
-        }).then(this.joinCall())
-    }
+        //     navigator.mediaDevices.getUserMedia({ audio: true, video: true })
+        //         .then(stream => {
+        //             this.localStream = stream;
+        //             this.video.srcObject = stream;
+        //         }).catch(error => { console.log(error) })
+        // }).then(this.joinCall())
+
+          navigator.mediaDevices.getUserMedia({ audio: true, video: true })
+            .then((stream) => {
+              this.localStream = stream;
+              // Older browsers may not have srcObject
+              if ("srcObject" in this.video) {
+                this.video.srcObject = stream
+              } else {
+                // Avoid using this in new browsers, as it is going away.
+                this.video.src = window.URL.createObjectURL(stream)
+              }
+              this.video.onloadedmetadata = (e) => {
+                this.video.play()
+              }
+            }).then(() => this.joinCall())
+            .catch((err) => {
+              console.log(err.name + ": " + err.message);
+            });
+    
+  })
+}
 
 
 
