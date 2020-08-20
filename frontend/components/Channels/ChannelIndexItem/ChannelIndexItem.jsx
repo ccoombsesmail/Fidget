a88c7c73c95d52a4bc4b1fca3624a3030056deb2
Charles Coombs-Esmail <coombs.charles@gmail.com>
2020-08-19 23:42:32 -0700

    Direct to videos instead of channel home for top channels

--- frontend/components/Channels/ChannelIndexItem/ChannelIndexItem.jsx
+++ frontend/components/Channels/ChannelIndexItem/ChannelIndexItem.jsx
@@ -1,36 +1,33 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import classes from './ChannelIndexItem.module.css'
-import {Link} from 'react-router-dom'
 /* eslint-disable */
 
-const ChannelIndexItem = ({user, vod, channel}) => {
-
-
-    return (
-
-        <div>
-        {user ? 
-        <Link key={vod.id} to={{pathname: `/channels/${vod.channelId}/${channel.channelName}/home` }}> 
-        <div id = "channelIndexItem" className={classes.channelItem}> 
-            {  
-            vod.videoUrl ? (
-                <video className={classes.videoPlayer}>
-                        <source src={vod.videoUrl} /> 
-                </video>) : (<img className={classes.icon} src={channel.logoUrl} />  )
-            }
-        </div>
-        <div className = {classes.channelDetailsWrapper}>
-            <img className={classes.icon} src={channel.logoUrl} />
-            <section className = {classes.details}> 
-                <h4>{vod.title}</h4>
-                <h5>{user.username}</h5> 
-                <h5>{vod.category}</h5> 
-            </section>
-        </div>
-        </Link> : null
-            }
-        </div>
-    )
+const ChannelIndexItem = ({ user, vod, channel }) => {
+  return (
+    <div>
+      {user ? (
+      <Link key={vod.id} to={{pathname: `/channels/${vod.channelId}/${channel.channelName}/videos` }}> 
+      <div id = "channelIndexItem" className={classes.channelItem}> 
+        {  
+        vod.videoUrl ? (
+          <video className={classes.videoPlayer}>
+            <source src={vod.videoUrl} /> 
+          </video>) : (<img className={classes.icon} src={channel.logoUrl} />  )
+        }
+      </div>
+      <div className = {classes.channelDetailsWrapper}>
+          <img className={classes.icon} src={channel.logoUrl} />
+          <section className = {classes.details}> 
+            <h4>{vod.title}</h4>
+            <h5>{user.username}</h5> 
+            <h5>{vod.category}</h5> 
+          </section>
+      </div>
+      </Link> ) : null
+          }
+    </div>
+  )
 }
 
 
--- frontend/components/HomePageIndex/HomePageIndex.jsx
+++ frontend/components/HomePageIndex/HomePageIndex.jsx
@@ -5,42 +5,25 @@ import Categories from '../Categories/Categories'
 import ChannelIndex from '../Channels/ChannelIndex'
 import Carousel from '../Carousel/Carousel'
 
-class HomePageIndex extends React.Component {
+const HomePageIndex = () => {
 
-    constructor(props) {
-        super(props)
-    }
-
-    render() {
-
-        return (
-
-            <div id="indexArea" className={classes.indexWrapper}>
-            <Carousel></Carousel>
-
-                <h2> Top Channels </h2>
-                <hr/>
-                <div className={classes.innerContainer}>
-                    <ChannelIndex/>
-                    <hr/>
-                    <h2 className = {classes.categoriesHeader}> <b>Categories</b> You May Like</h2>
-                    <Categories />
-                </div>
-
-            </div>
-
-
-        )
-    }
-
+  return (
+    <div id="indexArea" className={classes.indexWrapper}>
+      <Carousel />
+      <h2> Top Channels </h2>
+      <hr />
+      <div className={classes.innerContainer}>
+        <ChannelIndex />
+        <hr />
+        <h2 className={classes.categoriesHeader}>
+          <b>Categories</b>
+          You May Like
+        </h2>
+        <Categories />
+      </div>
+    </div>
+  )
 }
 
 
 export default HomePageIndex
-
-
-
-
-
-
-
