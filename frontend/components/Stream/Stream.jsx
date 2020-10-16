3f51068586dff9505f670f8a6645163a8bae7493
Charles Coombs-Esmail <coombs.charles@gmail.com>
2020-10-15 21:41:41 -0400

    Removing some old code

--- frontend/components/Stream/Stream.jsx
+++ frontend/components/Stream/Stream.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable */
 
 import React from 'react'
 import { connect } from 'react-redux'
@@ -43,12 +42,6 @@ class Stream extends React.Component {
 
       this.video = document.getElementById("local-video")
       this.props.requestChannel(this.props.match.params.channelId).then(() => {
-      //     navigator.mediaDevices.getUserMedia({ audio: true, video: true })
-      //         .then(stream => {
-      //             this.localStream = stream;
-      //             this.video.srcObject = stream;
-      //         }).catch(error => { console.log(error) })
-      // }).then(this.joinCall())
 
       navigator.mediaDevices.getUserMedia({ audio: true, video: true })
         .then((stream) => {
